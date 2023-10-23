0cfe76c24c458d0db8c1724983e8976bfccdd79c
imsamet <46278098+imsamet@users.noreply.github.com>
2023-10-23 11:07:08 +0300

    components fixed, hamburger menu created

--- components/nav/index.js
+++ components/nav/index.js
@@ -4,17 +4,75 @@ import Image from 'next/image';
 import useScrollPosition from '@/hooks/useScrollPosition';
 import Button from '../elements/button';
 import MenuButton from '../elements/menuButton';
+import { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 const Nav = () => {
+  const router = useRouter();
   const scroll = useScrollPosition();
+  const [isOpenMenu, setOpenMenu] = useState(false);
+  const toggle = () => {
+    setOpenMenu(prev => !prev);
+  };
+  const items = [
+    {
+      label: 'Homepage',
+      href: '/',
+      subItem: [
+        { label: 'How it work', href: '#' },
+        { label: 'Benefits', href: '#' },
+        { label: 'Complete solution', href: '#' },
+        { label: 'About us', href: '#' },
+        { label: 'NeoInsurance Marketplace', href: '#' },
+        { label: 'How to join', href: '#' },
+      ],
+    },
+    { label: 'Market Insights', href: '#' },
+    { label: 'Get in touch', href: '#' },
+  ];
   return (
-    <nav className={cn(styles.nav, scroll > 5 && styles.scroll)}>
+    <nav className={cn(styles.nav, scroll > 5 && styles.scroll, isOpenMenu && styles.close)}>
       <div className="container">
-        <div className={styles.content}>
+        <div className={cn(styles.content, isOpenMenu && styles.close)}>
           <Image src="/logo-white.png" className={styles.logo} height="30" width="189" />
           <div className={styles.buttonBox}>
-            <Button>Request a demo</Button>
-            <MenuButton />
+            <Button size="medium" variant="bordered" className={styles.contactButton}>
+              Request a demo
+            </Button>
+            <MenuButton onClick={toggle} />
           </div>
+        </div>
+      </div>
+      <div onClick={toggle} className={cn(styles.hamburberMenu, isOpenMenu && styles.open)}>
+        <div className={styles.menuContent}>
+          <ul className={styles.list}>
+            {items?.map(i => (
+              <li className={styles.item}>
+                <Link className={cn(styles.link, router.pathname === i?.href && styles.active)} href={i?.href}>
+                  {i?.label}
+                </Link>
+                {i?.subItem && (
+                  <>
+                    <ul className={styles.subList}>
+                      {i?.subItem?.map(s => (
+                        <li className={styles.subItem}>
+                          <Link
+                            className={cn(styles.link, styles.subLink, router.asPath === s?.href && styles.active)}
+                            href={s?.href}
+                          >
+                            {s?.label}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+              </li>
+            ))}
+          </ul>
+          <Button size="large" variant="colored" className={cn(styles.contactButton, styles.hamburgerContactButton)}>
+            Request a demo
+          </Button>
         </div>
       </div>
     </nav>
