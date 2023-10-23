23f4d51b7ffdc4524f839b3c721b86e2174633ea
imsamet <46278098+imsamet@users.noreply.github.com>
2023-10-23 11:30:41 +0300

    build fix

--- components/elements/texts/paragraph/index.js
+++ components/elements/texts/paragraph/index.js
@@ -1,7 +1,7 @@
 import cn from 'classnames';
-import Style from './style.module.scss';
+import Style from './style.module.css';
 import PropTypes from 'prop-types';
-const Paragraph = ({ type = "p1", className, children }) => {
+const Paragraph = ({ type = 'p1', className, children }) => {
   return <p className={cn(Style.paragraph, Style[type], className)}>{children}</p>;
 };
 Paragraph.propType = {
