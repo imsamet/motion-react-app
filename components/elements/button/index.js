7c2c0945a70801dd3133ea16bf21d46d5a014bcb
imsamet <46278098+imsamet@users.noreply.github.com>
2023-10-23 11:02:33 +0300

    added button variants

--- components/elements/button/index.js
+++ components/elements/button/index.js
@@ -1,12 +1,21 @@
+import PropTypes from 'prop-types';
 import Icons from '@/components/icons/icons';
 import styles from './style.module.css';
 import cn from 'classnames';
-const Button = ({ children, className, ...props }) => {
+const Button = ({ variant, size, children, className, ...props }) => {
   return (
-    <button className={cn(styles.button, className)} {...props}>
+    <button className={cn(styles.button, styles?.[size], styles?.[variant], className)} {...props}>
       <span className={styles.text}>{children}</span>
       <Icons className={styles.icon} name="Left" />
     </button>
   );
+};
+Button.propTypes = {
+  size: PropTypes.oneOf('medium', 'large'),
+  variant: PropTypes.oneOf('bordered', 'colored'),
+};
+Button.defaultProps = {
+  size: 'medium',
+  variant: 'bordered',
 };
 export default Button;
