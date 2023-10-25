b3bcb2db4c5a43f4198ab26b2e0cc15e8790e4ea
imsamet <46278098+imsamet@users.noreply.github.com>
2023-10-25 17:52:18 +0300

    added custom media, added postcss plugins and fixed tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   plugins: {
+    '@csstools/postcss-global-data': {
+      files: ['styles/_mixins.css', 'styles/_theme.css'],
+    },
+    'postcss-preset-env': {
+      features: {
+        'custom-media-queries': true,
+        'custom-properties': false,
+        'nesting-rules': true,
+      },
+    },
     'postcss-nested': {},
     'postcss-custom-media': {},
     'tailwindcss/nesting': {},
