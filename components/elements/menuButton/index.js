4ca604e1a8f9c20b70349658ecd2120a633b3d9c
imsamet <46278098+imsamet@users.noreply.github.com>
2023-10-26 14:06:30 +0300

    The new icon was used in relevant places and corrections were made

--- components/elements/menuButton/index.js
+++ components/elements/menuButton/index.js
@@ -1,11 +1,18 @@
+import PropTypes from 'prop-types';
 import Icons from '@/components/icons/icons';
 import styles from './style.module.css';
 import cn from 'classnames';
-const MenuButton = ({ children, className, ...props }) => {
+const MenuButton = ({ type, children, className, ...props }) => {
   return (
     <button className={cn(styles.menu, className)} {...props}>
-      <Icons width="60" height="60" className={styles.icon} name="Menu" />
+      <Icons width="60" height="60" className={styles.icon} name={type} />
     </button>
   );
+};
+MenuButton.propTypes = {
+  type: PropTypes.oneOf(['menu', 'close']),
+};
+MenuButton.defaultProps = {
+  type: 'menu',
 };
 export default MenuButton;
--- components/nav/index.js
+++ components/nav/index.js
@@ -11,7 +11,8 @@ const Nav = () => {
   const router = useRouter();
   const scroll = useScrollPosition();
   const [isOpenMenu, setOpenMenu] = useState(false);
-  const toggle = () => {
+  const toggle = e => {
+    e.stopPropagation();
     setOpenMenu(prev => !prev);
   };
   const isOtherPage = router.pathname !== '/';
@@ -57,35 +58,40 @@ const Nav = () => {
         </div>
       </div>
       <div onClick={toggle} className={cn(styles.hamburberMenu, isOpenMenu && styles.open)}>
-        <div className={styles.menuContent}>
-          <ul className={styles.list}>
-            {items?.map((i, key) => (
-              <li key={key} className={styles.item}>
-                <Link className={cn(styles.link, router.pathname === i?.href && styles.active)} href={i?.href}>
-                  {i?.label}
-                </Link>
-                {i?.subItem && (
-                  <>
-                    <ul className={styles.subList}>
-                      {i?.subItem?.map((s, sKey) => (
-                        <li key={sKey} className={styles.subItem}>
-                          <Link
-                            className={cn(styles.link, styles.subLink, router.asPath === s?.href && styles.active)}
-                            href={s?.href}
-                          >
-                            {s?.label}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </>
-                )}
-              </li>
-            ))}
-          </ul>
-          <Button size="large" variant="colored" className={cn(styles.contactButton, styles.hamburgerContactButton)}>
-            Request a demo
-          </Button>
+        <div onClick={e => e.stopPropagation()} className={styles.menuContent}>
+          <div>
+            <ul className={styles.list}>
+              {items?.map((i, key) => (
+                <li key={key} className={styles.item}>
+                  <Link className={cn(styles.link, router.pathname === i?.href && styles.active)} href={i?.href}>
+                    {i?.label}
+                  </Link>
+                  {i?.subItem && (
+                    <>
+                      <ul className={styles.subList}>
+                        {i?.subItem?.map((s, sKey) => (
+                          <li key={sKey} className={styles.subItem}>
+                            <Link
+                              className={cn(styles.link, styles.subLink, router.asPath === s?.href && styles.active)}
+                              href={s?.href}
+                            >
+                              {s?.label}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </>
+                  )}
+                </li>
+              ))}
+            </ul>
+            <Button size="large" variant="colored" className={cn(styles.contactButton, styles.hamburgerContactButton)}>
+              Request a demo
+            </Button>
+          </div>
+          <div>
+            <MenuButton type="close" onClick={toggle} className={styles.close} />
+          </div>
         </div>
       </div>
     </nav>
