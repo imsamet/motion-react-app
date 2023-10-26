69d23e1aa6fc50bc359436813fb31fa3d6a4aa10
imsamet <46278098+imsamet@users.noreply.github.com>
2023-10-26 14:10:01 +0300

    created contact us form and added to the FormHero component

---
+++ components/forms/contactUs/index.js
@@ -0,0 +1,6 @@
+import cn from 'classnames';
+import styles from './styles.module.css';
+const ContactUs = ({}) => {
+  return <form></form>;
+};
+export default ContactUs;
--- components/heros/formHero/index.js
+++ components/heros/formHero/index.js
@@ -1,12 +1,21 @@
 import cn from 'classnames';
 import styles from './style.module.css';
+import Heading from '@/components/elements/texts/heading';
+import ContactUs from '@/components/forms/contactUs';
 const FormHero = () => {
   return (
     <header>
       <div className="container">
         <div className="row">
-          <div className={cn('col-12 col-xs-8', styles.red)}>col-8</div>
-          <div className="col-12 col-xs-4">col-4</div>
+          <div className="col-12 col-xs-8">
+            <span className={styles.heading}>Connect with us</span>
+            <Heading colored="Discover" className={styles.title} type="h1">
+              Discover our open insurance platform today!
+            </Heading>
+          </div>
+          <div className="col-12 col-xs-4">
+            <ContactUs />
+          </div>
         </div>
       </div>
     </header>
