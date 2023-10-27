9f4368c41fb660ebfa7b9c61de93ec3792b18f62
imsamet <46278098+imsamet@users.noreply.github.com>
2023-10-27 16:49:12 +0300

    updated button

--- components/elements/button/index.js
+++ components/elements/button/index.js
@@ -2,10 +2,28 @@ import PropTypes from 'prop-types';
 import Icons from '@/components/icons/icons';
 import styles from './style.module.css';
 import cn from 'classnames';
-const Button = ({ color, variant, size, children, className, ...props }) => {
+import Text from '../texts/text';
+const Button = ({ color, variant, size, isGradiend, children, className, ...props }) => {
   return (
-    <button className={cn(styles.button, styles?.[size], styles?.[variant], styles?.[color], className)} {...props}>
-      <span className={styles.text}>{children}</span>
+    <button
+      className={cn(
+        styles.button,
+        styles?.[size],
+        styles?.[variant],
+        styles?.[color],
+        isGradiend && styles.gradiend,
+        className,
+      )}
+      {...props}
+    >
+      <Text
+        tag="span"
+        size={size === 'medium' ? 'sm' : size === 'large' && '2xl'}
+        color={color}
+        className={styles.text}
+      >
+        {children}
+      </Text>
       <Icons className={styles.icon} name="left" />
     </button>
   );
@@ -14,6 +32,7 @@ Button.propTypes = {
   size: PropTypes.oneOf(['medium', 'large']),
   variant: PropTypes.oneOf(['bordered', 'colored']),
   color: PropTypes.oneOf(['white', 'black']),
+  isGradiend: PropTypes.bool,
 };
 Button.defaultProps = {
   size: 'medium',
