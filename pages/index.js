f1358a0cefa8880b44e0fd2a7e595cacfdaf458c
imsamet <46278098+imsamet@users.noreply.github.com>
2023-11-21 17:43:24 +0300

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home({}) {
-  return <div style={{ minHeight: '200vh', background: 'var(--dark-blue)' }}></div>;
+  return <div style={{ minHeight: '200vh', background: 'white' }}></div>;
 }
